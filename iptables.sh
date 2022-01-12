e348f524dc8030785232438426302d1fa1540646
Paweł Ciejka <pawel.ciejka@robco.pl>
2022-01-12 18:30:53 +0000

    added safety fuses

--- iptables.sh
+++ iptables.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-ip rule add fwmark 1 lookup 100
-ip route add local 0.0.0.0/0 dev lo table 100
-iptables -t mangle -A PREROUTING -p tcp -s 192.168.74.10/32 --sport 2598 -j MARK --set-xmark 0x1/0xffffffff
-iptables -t mangle -A PREROUTING -p tcp -s 192.168.74.10/32 --sport 443  -j MARK --set-xmark 0x1/0xffffffff
+if ! command -v ip &> /dev/null
+then
+    echo "command: ip could not be found"
+    exit
+fi
+
+if ! command -v iptables &> /dev/null
+then
+    echo "iptables could not be found"
+    exit
+fi
+
+
+ip rule add fwmark 1 lookup 100 || { echo "Failed to add fwmark"; exit }
+ip route add local 0.0.0.0/0 dev lo table 100 || { echo "Failed to add local route"; exit }
+iptables -t mangle -A PREROUTING -p tcp -s 192.168.74.10/32 --sport 2598 -j MARK --set-xmark 0x1/0xffffffff || { echo "Failed to add rule for port 2598"; exit }
+iptables -t mangle -A PREROUTING -p tcp -s 192.168.74.10/32 --sport 443  -j MARK --set-xmark 0x1/0xffffffff || { echo "Failed to add rule for port 443"; exit }
